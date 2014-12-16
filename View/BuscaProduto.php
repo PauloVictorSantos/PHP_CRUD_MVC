71b3d5b3485bd11bc2d7ee577dda68e5e33f33cb
victorpavicto@hotmail.com <8820paulo66041996>
2014-12-16 18:02:29 -0300

    Revert "Aplicação CRUD em PHP, usando MVC."
    This reverts commit cdef606080a61f7b2fe01b2d70e66d14cc37a704.

--- View/BuscaProduto.php
+++
@@ -1,9 +0,0 @@
-<div class="busca">
-	<form action="" method="post">
-	<div class="input-append">
-			<label>Busca:</label>
-			<input type="text" name="busca"  id="appendedInputButton" placeholder="Digite o nome do produto" />
-			<input class="btn" type="submit" name="button" value="Buscar"/>
-	</div>
-	</form>
-</div>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	require_once($_SERVER['DOCUMENT_ROOT'].'/PHP_sist/Controller/Sistema.php');
-	$sistema= new Sistema();
-	$sistema->SistemaAll();
-?>
